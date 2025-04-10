61f2f2db48ac32d5c08750ea14ed2784bb5cc0ac
Thant Ko Zaw <thantmkou10@gmail.com>
2025-04-10 08:53:06 +0000

    implemented feature 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { add } from "../../testing-submodule-feature-1/add";
+import { subtract } from "../../testing-submodule-feature-2/subtract";
 
 export default function Home() {
-  return <div>{add(1, 2)}</div>;
+  return (
+    <>
+      <div>{add(1, 2)}</div>
+      <div>{subtract(5, 1)}</div>
+    </>
+  );
 }
